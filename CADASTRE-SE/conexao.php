e13a0aa347caa5a264229721ecc87a04db8bddde
Luana <125032383+dev-luanA@users.noreply.github.com>
2023-09-29 14:07:19 -0300

    inserção de cadastro

--- CADASTRE-SE/conexao.php
+++ CADASTRE-SE/conexao.php
@@ -1,8 +1,37 @@
-<?php 
+<?php
+$conn = new mysqli("localhost", "root", "", "db_healthsync");
 
-$conn = new mysqli ("localhost","root","","bd_healthsync");
-        // verifica se a conexão foi bem-sucedida
-    if ($conn->connect_error) {
-        die ("Erro de conexão". $conn->connect_error);}
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+   
+
+   
+    $stmt = $conn->prepare("INSERT INTO funcionario (funcionario_nome, funcionario_email, funcionario_senha) VALUES (?, ?, ?)");
+
+    if ($stmt === false) {
+        die("Erro de preparação da declaração: " . $conn->error);
+    }
+
+    
+    $stmt->bind_param("sss", $nome, $email, $senha);
+
+    if ($stmt->execute()) {
+        header("Location: http://localhost/Healthsync/");
+        exit();
+    } else {
+        die("Erro ao inserir dados: " . $stmt->error);
+    }
+
+   
+    $stmt->close();
+}
+
+
+$conn->close();
 ?>
--- REDEFINIR SENHA/conexao.php
+++ REDEFINIR SENHA/conexao.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = new mysqli ("localhost","root","","bd_healthsync");
+$conn = new mysqli ("localhost","root","","db_healthsync");
         // verifica se a conexão foi bem-sucedida
     if ($conn->connect_error) {
         die ("Erro de conexão". $conn->connect_error);}
